be82c6591a918b2788641aadf94c2a3cda954ede
Arunabh Das <arunabhdas@gmail.com>
2023-02-13 03:24:40 -0500

    Added CompanyItem composable

---
+++ unicornstack-android/unicorn-android/app/src/main/java/app/unicornapp/mobile/android/unicorn/presentation/company_listings/CompanyListingsScreen.kt
@@ -0,0 +1,10 @@
+package app.unicornapp.mobile.android.unicorn.presentation.company_listings
+
+/**
+ * CompanyListingsScreen
+ */
+class CompanyListingsScreen(
+    navigator: DestinationsNavigator
+) {
+
+}
