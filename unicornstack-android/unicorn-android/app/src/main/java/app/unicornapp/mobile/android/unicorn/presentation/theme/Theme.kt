5a857243119bf9d2724f4bff6adfff0dfc1130a9
Arunabh Das <arunabhdas@gmail.com>
2023-02-17 05:21:50 -0500

    Cleanup Theme

--- unicornstack-android/unicorn-android/app/src/main/java/app/unicornapp/mobile/android/unicorn/presentation/theme/Theme.kt
+++ unicornstack-android/unicorn-android/app/src/main/java/app/unicornapp/mobile/android/unicorn/presentation/theme/Theme.kt
@@ -2,6 +2,7 @@ package app.unicornapp.mobile.android.unicorn.presentation.theme
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Shapes
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -14,10 +15,14 @@ import androidx.compose.ui.platform.LocalView
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
+    background = DarkBlue,
+    onPrimary = Color.DarkGray,
+    onBackground = TextWhite,
     secondary = PurpleGrey80,
     tertiary = Pink80
 )
 
+/* TODO-FIXME-DEPRECATED
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
@@ -30,7 +35,7 @@ private val LightColorScheme = lightColorScheme(
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
 )
-
+*/
 @Composable
 fun UnicornTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
@@ -38,26 +43,8 @@ fun UnicornTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-    val view = LocalView.current
-    /* TODO-FIXME
-    if (!view.isInEditMode) {
-        SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
-        }
-    }
-    */
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = DarkColorScheme,
         typography = Typography,
         content = content
     )
